2d2f2db44163d8d14c625b212556cf05de23a6bd
lucid-techwrld <davisredtiger@gmail.com>
2025-04-19 11:26:20 -0700

    increase offercard height

--- src/components/offerCard.jsx
+++ src/components/offerCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const OfferCard = ({ head, icon, text }) => {
   return (
     <>
-      <div className='w-full h-auto bg-orange-400 border-2 border-white p-2 rounded-se-3xl text-sm md:text-xl lg:text-xl relative text-white hover:-translate-y-1 transition-all'>
+      <div className='w-full h-24 bg-orange-400 border-2 border-white p-2 rounded-se-3xl text-sm md:text-xl lg:text-xl relative text-white hover:-translate-y-1 transition-all'>
         <div className=''>
           <h2 className='font-bold text-xl text-black'>{head}</h2>
           <p className='text-xs lg:text-sm'>{text}</p>
